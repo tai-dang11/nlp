62f460e9827be8086107ab6358a08e834988038e
tai-dang11 <taithe.dang11@gmail.com>
2021-12-28 05:26:13 -0800

    github

--- main.py
+++ main.py
@@ -1,52 +1,161 @@
+from transformers import Trainer, AlbertTokenizer, TFAlbertForQuestionAnswering, TrainingArguments
 import tensorflow as tf
-from datasets import load_dataset
 from transformers import AutoTokenizer
-from transformers import DataCollatorWithPadding
-from transformers import create_optimizer
-from transformers import TFAutoModelForSequenceClassification
+from datasets import load_dataset
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-model = TFAutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
+squad = load_dataset("squad")
+tokenizer = AutoTokenizer.from_pretrained('albert-base-v2')
+model = TFAlbertForQuestionAnswering.from_pretrained('albert-base-v2')
 
-imdb = load_dataset("imdb")
-
-def preprocess_function(examples):
-    return tokenizer(examples["text"], truncation=True)
-
-tokenized_imdb = imdb.map(preprocess_function, batched=True)
-
-data_collator = DataCollatorWithPadding(tokenizer, return_tensors="tf")
-#/Users/dttai11/.cache/huggingface/datasets/imdb
+import transformers
+squad_v2 = False
+# model_checkpoint = "distilbert-base-uncased"
 batch_size = 16
+from datasets import load_dataset, load_metric
+datasets = load_dataset("squad_v2" if squad_v2 else "squad")
 
-tf_train_dataset = tokenized_imdb["train"].to_tf_dataset(
-    columns=['attention_mask', 'input_ids', 'label'],
+from datasets import ClassLabel, Sequence
+import random
+import pandas as pd
+
+
+# def show_random_elements(dataset, num_examples=10):
+#     assert num_examples <= len(
+#         dataset
+#     ), "Can't pick more elements than there are in the dataset."
+#     picks = []
+#     for _ in range(num_examples):
+#         pick = random.randint(0, len(dataset) - 1)
+#         while pick in picks:
+#             pick = random.randint(0, len(dataset) - 1)
+#         picks.append(pick)
+#
+#     df = pd.DataFrame(dataset[picks])
+#     for column, typ in dataset.features.items():
+#         if isinstance(typ, ClassLabel):
+#             df[column] = df[column].transform(lambda i: typ.names[i])
+#         elif isinstance(typ, Sequence) and isinstance(typ.feature, ClassLabel):
+#             df[column] = df[column].transform(
+#                 lambda x: [typ.feature.names[i] for i in x]
+#             )
+#     display(HTML(df.to_html()))
+pad_on_right = tokenizer.padding_side == "right"
+max_length = 384  # The maximum length of a feature (question and context)
+doc_stride = 128
+def prepare_train_features(examples):
+    # Tokenize our examples with truncation and padding, but keep the overflows using a stride. This results
+    # in one example possible giving several features when a context is long, each of those features having a
+    # context that overlaps a bit the context of the previous feature.
+    tokenized_examples = tokenizer(
+        examples["question" if pad_on_right else "context"],
+        examples["context" if pad_on_right else "question"],
+        truncation="only_second" if pad_on_right else "only_first",
+        max_length=max_length,
+        stride=doc_stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    # Since one example might give us several features if it has a long context, we need a map from a feature to
+    # its corresponding example. This key gives us just that.
+    sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+    # The offset mappings will give us a map from token to character position in the original context. This will
+    # help us compute the start_positions and end_positions.
+    offset_mapping = tokenized_examples.pop("offset_mapping")
+
+    # Let's label those examples!
+    tokenized_examples["start_positions"] = []
+    tokenized_examples["end_positions"] = []
+
+    for i, offsets in enumerate(offset_mapping):
+        # We will label impossible answers with the index of the CLS token.
+        input_ids = tokenized_examples["input_ids"][i]
+        cls_index = input_ids.index(tokenizer.cls_token_id)
+
+        # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+        sequence_ids = tokenized_examples.sequence_ids(i)
+
+        # One example can give several spans, this is the index of the example containing this span of text.
+        sample_index = sample_mapping[i]
+        answers = examples["answers"][sample_index]
+        # If no answers are given, set the cls_index as answer.
+        if len(answers["answer_start"]) == 0:
+            tokenized_examples["start_positions"].append(cls_index)
+            tokenized_examples["end_positions"].append(cls_index)
+        else:
+            # Start/end character index of the answer in the text.
+            start_char = answers["answer_start"][0]
+            end_char = start_char + len(answers["text"][0])
+
+            # Start token index of the current span in the text.
+            token_start_index = 0
+            while sequence_ids[token_start_index] != (1 if pad_on_right else 0):
+                token_start_index += 1
+
+            # End token index of the current span in the text.
+            token_end_index = len(input_ids) - 1
+            while sequence_ids[token_end_index] != (1 if pad_on_right else 0):
+                token_end_index -= 1
+
+            # Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
+            if not (
+                offsets[token_start_index][0] <= start_char
+                and offsets[token_end_index][1] >= end_char
+            ):
+                tokenized_examples["start_positions"].append(cls_index)
+                tokenized_examples["end_positions"].append(cls_index)
+            else:
+                # Otherwise move the token_start_index and token_end_index to the two ends of the answer.
+                # Note: we could go after the last offset if the answer is the last word (edge case).
+                while (
+                    token_start_index < len(offsets)
+                    and offsets[token_start_index][0] <= start_char
+                ):
+                    token_start_index += 1
+                tokenized_examples["start_positions"].append(token_start_index - 1)
+                while offsets[token_end_index][1] >= end_char:
+                    token_end_index -= 1
+                tokenized_examples["end_positions"].append(token_end_index + 1)
+
+    return tokenized_examples
+
+
+tokenized_datasets = datasets.map(
+    prepare_train_features, batched=True, remove_columns=datasets["train"].column_names
+)
+
+learning_rate = 2e-5
+num_train_epochs = 2
+weight_decay = 0.01
+from  transformers.data.data_collator import tf_default_data_collator
+# from transformers.data.data_collator import
+from transformers import DefaultDataCollator
+
+# data_collator = tf_default_data_collator(return_tensors="tf")
+data_collator = tf_default_data_collator
+train_set = tokenized_datasets["train"].to_tf_dataset(
+    columns=["attention_mask", "input_ids", "start_positions", "end_positions"],
     shuffle=True,
     batch_size=batch_size,
     collate_fn=data_collator,
 )
-
-tf_validation_dataset = tokenized_imdb["train"].to_tf_dataset(
-    columns=['attention_mask', 'input_ids', 'label'],
+validation_set = tokenized_datasets["validation"].to_tf_dataset(
+    columns=["attention_mask", "input_ids", "start_positions", "end_positions"],
     shuffle=False,
     batch_size=batch_size,
     collate_fn=data_collator,
 )
 
-num_epochs = 5
-batches_per_epoch = len(tokenized_imdb["train"]) // batch_size
-# batches_per_epoch = 256
-total_train_steps = int(batches_per_epoch * num_epochs)
+from transformers import create_optimizer
+
+total_train_steps = (len(tokenized_datasets["train"]) // batch_size) * num_train_epochs
+
 optimizer, schedule = create_optimizer(
-    init_lr=2e-5,
-    num_warmup_steps=0,
-    num_train_steps=total_train_steps
+    init_lr=learning_rate, num_warmup_steps=0, num_train_steps=total_train_steps
 )
+
 
 model.compile(optimizer=optimizer)
 
-model.fit(
-    tf_train_dataset,
-    validation_data=tf_validation_dataset,
-    epochs=1,
-)
+model.fit(train_set, validation_data=validation_set, epochs=1)
