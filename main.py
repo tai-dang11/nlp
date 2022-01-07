c2b96b744f0f141a08d2ad9cde06d8444c740147
tai-dang11 <taithe.dang11@gmail.com>
2022-01-07 06:48:35 -0800

    github

--- main.py
+++ main.py
@@ -1,42 +1,100 @@
-import tensorflow_hub as hub
-# import tensorflow as tf
-# layer = hub.load("/Users/dttai11/albert/trainingTest/albert_base_2")
-# layer = "https://tfhub.dev/google/albert_base/2"
-# layer = hub.load("https://tfhub.dev/google/albert_base/2")
-# hub_layer = hub.KerasLayer("/Users/dttai11/albert/trainingTest/albert_base_2")
-# model = tf.keras.Sequential([hub_layer])
-# model.summary()
-
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
+from transformers import AlbertTokenizer, TFAlbertForQuestionAnswering
 import tensorflow as tf
+from datasets import load_dataset
+squad = load_dataset("squad")
+from transformers import AutoTokenizer
 
-tags = set()
-# if is_training:
-#   tags.add("train")
-# albert_module = hub.load("https://tfhub.dev/google/albert_base/1", tags=tags,
-#                            )
-#
-# albert_module.summary()
-input_ids = tf.keras.layers.Input(shape=[None], dtype=tf.int32)
-input_mask = tf.keras.layers.Input(shape=[None], dtype=tf.int32)
-sequence_mask = tf.keras.layers.Input(shape=[None], dtype=tf.int32)
-# albert = hub.KerasLayer(
-#     "https://tfhub.dev/google/albert_base/3",
-#     trainable=True,
-#     signature="tokens",
-#     output_key="pooled_output",
-# )
-albert = tf.keras.Sequential([hub.KerasLayer("https://tfhub.dev/google/albert_base/2",signature='tokens' , signature_outputs_as_dict=True, trainable=True)])
-features = {
-    "input_ids": input_ids,
-    "input_mask": input_mask,
-    "segment_ids": sequence_mask,
-}
+tokenizer = AutoTokenizer.from_pretrained('albert-base-v2')
+model = TFAlbertForQuestionAnswering.from_pretrained('albert-base-v2')
 
-out = albert(features)
-model = tf.keras.Model(inputs=[input_ids, input_mask, sequence_mask], outputs=out)
-# model.compile("adam", loss="sparse_categorical_crossentropy")
-# model.summary()
-albert.summary()
-# print(f"The Hub encoder has {len(albert.trainable_variables)} trainable variables")
+
+def preprocess_function(examples):
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=384,
+        truncation="only_second",
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    answers = examples["answers"]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        answer = answers[i]
+        start_char = answer["answer_start"][0]
+        end_char = answer["answer_start"][0] + len(answer["text"][0])
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end of the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # If the answer is not fully inside the context, label it (0, 0)
+        if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return inputs
+
+tokenized_squad = squad.map(preprocess_function, batched=True, remove_columns=squad["train"].column_names)
+
+from transformers.data.data_collator import tf_default_data_collator
+data_collator = tf_default_data_collator
+
+tf_train_set = tokenized_squad["train"].to_tf_dataset(
+    columns=["attention_mask", "input_ids", "start_positions", "end_positions"],
+    dummy_labels=True,
+    shuffle=True,
+    batch_size=16,
+    collate_fn=data_collator,
+)
+
+tf_validation_set = tokenized_squad["validation"].to_tf_dataset(
+    columns=["attention_mask", "input_ids", "start_positions", "end_positions"],
+    dummy_labels=True,
+    shuffle=False,
+    batch_size=16,
+    collate_fn=data_collator,
+)
+
+from transformers import create_optimizer
+
+batch_size = 16
+num_epochs = 2
+total_train_steps = (len(tokenized_squad["train"]) // batch_size) * num_epochs
+optimizer, schedule = create_optimizer(
+    init_lr=2e-5,
+    num_warmup_steps=0,
+    num_train_steps=total_train_steps,
+)
+
+model.compile(optimizer=optimizer)
+
+model.fit(
+    tf_train_set,
+    validation_data=tf_validation_set,
+    epochs=1,
+)
