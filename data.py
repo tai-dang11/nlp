e1b0313c60b77ea1be32636da8b03d9946131993
tai-dang11 <taithe.dang11@gmail.com>
2022-01-26 05:06:45 -0500

    github

--- data.py
+++ data.py
@@ -3,5 +3,4 @@ from pprint import pprint
 
 squad_train = load_dataset('squad_v2', split='train')
 squad_valid = load_dataset('squad_v2', split='validation')
-# print(squad_train)
 pprint(squad_train[10008])
